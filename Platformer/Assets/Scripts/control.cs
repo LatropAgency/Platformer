859009aa42f5beddff3c0e2a352b7b6c1a04a4ec
LatropAgency <vovakolesnikovich@gmail.com>
2019-04-27 17:39:55 +0300

    fdgd

---
+++ Platformer/Assets/Scripts/control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public float speed = 20;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveY = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
+        if (moveY!=0)
+            rb.MovePosition(rb.position + Vector2.up * moveY * speed * Time.deltaTime);
+        else
+            rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+    }
+}
